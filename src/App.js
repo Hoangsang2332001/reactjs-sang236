855139b53e5c27e001c67c082721e219f003de1b
Hoangsang2332001 <nhoksang2332@gmail.com>
2022-03-05 20:25:50 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo2 from './logo.svg';
+import logosang from './logo-sang.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logosang} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://hoangsang2332001.github.io/Sang-blog/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ảo thuật
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
